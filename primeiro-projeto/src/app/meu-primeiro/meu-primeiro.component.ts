c80738549953bef2877df98005ea08f1ad053dab
Matheus Martins <matheus.mmag@gmail.com>
2021-07-11 16:50:10 -0300

    Criação do primeiro componente

---
+++ primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'meu-primeiro-component',//tag html que a gente quer utilizar
+  template: `<p>Meu primeiro componente com angular ;)</p>`,
+})
+export class MeuPrimeiroComponent{
+
+}

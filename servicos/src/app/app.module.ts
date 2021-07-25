1e9f553d684e24116fad0aa9d0b8da5201e822b5
Matheus Martins <matheus.mmag@gmail.com>
2021-07-25 14:24:42 -0300

    37~41: Serviços

--- servicos/src/app/app.module.ts
+++ servicos/src/app/app.module.ts
@@ -1,16 +1,26 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CursosModule } from './cursos/cursos.module';
+import { CursosService } from './cursos/cursos.service';
+import { CriarCursoModule } from './criar-curso/criar-curso.module';
+import { LogService } from './shared/log.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CriarCursoModule,
+    CursosModule
   ],
-  providers: [],
+  providers: [
+    LogService,
+    CursosService
+  ],
+  //providers: [CursosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

c146883a96e85de3ef088cb05fb8430b5d2f7b64
Matheus Martins <matheus.mmag@gmail.com>
2021-07-31 15:45:11 -0300

    55: Rotas Imperativas: Redirecionamento via código

--- rotas/src/app/app-routing.module.ts
+++ rotas/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
 import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 import { CursosComponent } from './cursos/cursos.component';
 import { LoginComponent } from './login/login.component';
@@ -6,14 +7,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: 'cursos', component: CursosComponent},
-  {path: 'curso/:id', component: CursoDetalheComponent},
-  {path: 'login', component: LoginComponent},
-  {path: '', component: HomeComponent}
+  { path: 'cursos', component: CursosComponent },
+  { path: 'curso/:id', component: CursoDetalheComponent },
+  { path: 'naoEncontrado', component: CursoNaoEncontradoComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '', component: HomeComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

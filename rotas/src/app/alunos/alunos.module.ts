197eaeed486e6f1d383862e22d3b97d0fdc84aeb
Matheus Martins <matheus.mmag@gmail.com>
2021-07-31 20:54:20 -0300

    61: Rotas Filhas: desenvolvendo as telas

--- rotas/src/app/alunos/alunos.module.ts
+++ rotas/src/app/alunos/alunos.module.ts
@@ -1,3 +1,4 @@
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -5,11 +6,13 @@ import { AlunosRoutingModule } from './alunos.routing.module';
 import { AlunosComponent } from './alunos.component';
 import { AlunoFormComponent } from './aluno-form/aluno-form.component';
 import { AlunoDetalheComponent } from './aluno-detalhe/aluno-detalhe.component';
+import { AlunosService } from './alunos.service';
 
 @NgModule({
     imports: [
       CommonModule,
-      AlunosRoutingModule
+      AlunosRoutingModule,
+      FormsModule
     ],
     exports: [],
     declarations: [
@@ -17,6 +20,8 @@ import { AlunoDetalheComponent } from './aluno-detalhe/aluno-detalhe.component';
       AlunoFormComponent,
       AlunoDetalheComponent
     ],
-    providers: [],
+    providers: [
+      AlunosService
+    ],
 })
 export class AlunosModule { }

543c7a3a3e91e846d4f857d3b0bd35280dbbf782
Matheus Martins <matheus.mmag@gmail.com>
2021-08-05 21:03:53 -0300

    74: Forms (template driven) Controles ngForm, ngSubmit e ngModel

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template-form',
   templateUrl: './template-form.component.html',
-  styleUrls: ['./template-form.component.css']
+  styleUrls: ['./template-form.component.css'],
 })
 export class TemplateFormComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  onSubmit(formulario: NgForm) {
+    console.log(formulario);
   }
 
+  ngOnInit(): void {}
 }
